d3e666bd150534d9ca165175938a53736d49c04e
Tyler Hatch <hatch.tyler@gmail.com>
2022-12-29 07:42:12 -0800

    Add generate_wells function and format with black

--- generate_scenario_pumping.py
+++ generate_scenario_pumping.py
@@ -1,13 +1,15 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 
 def generate_pumping_column_references(
     projects: list,
     well_data: pd.DataFrame,
     well_names_file: str,
     col_name: str,
-    n_columns: int
+    n_columns: int,
 ) -> pd.DataFrame:
     """
     Generates column references for each project well
@@ -36,7 +38,7 @@ def generate_pumping_column_references(
     """
     # read well names and owners
     well_names = pd.read_csv(well_names_file)
-    
+
     # join well names and owners with well specification information
     well_data = well_data.join(well_names)
 
@@ -50,25 +52,59 @@ def generate_pumping_column_references(
     for proj in projects:
         proj_wells = well_data[well_data["Owner"] == proj].sort_values(by="ICOLWL")
         well_ids = proj_wells["ICOLWL"].tolist()
-        
+
         for wid in well_ids:
             well_data.loc[
                 (well_data["Owner"] == proj) & (well_data["ICOLWL"] == wid), col_name
             ] = current_col
             current_col += 1
-    
+
     return well_data
 
+
+def generate_wells(project: str, well_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generate new wells for project with scenario pumping
+
+    Parameters
+    ----------
+    project : str
+        project name or owner with wells
+
+    well_data : pd.DataFrame
+        pandas DataFrame containing well specifications
+
+    Returns
+    -------
+    pd.DataFrame
+        pandas DataFrame containing project well with scenario information
+    """
+    # create a copy of the wells for the project
+    proj_wells = well_data[well_data["Owner"] == project].copy().reset_index(drop=True)
+
+    # generate new IDs for the duplicated wells
+    proj_wells["ID"] = np.arange(
+        len(well_data) + 1, len(well_data) + len(proj_wells) + 1
+    )
+
+    # fill in values for other columns
+    proj_wells["ICOLWL"] = proj_wells["ICOLWL2"]
+    proj_wells["ICWLMAX"] = proj_wells["ICOLWL2"]
+    proj_wells["TYPDSTWL"] = 0
+    proj_wells["DSTWL"] = 0
+    proj_wells["ICFIRIGWL"] = 0
+
+    return proj_wells
+
+
 def generate_pumping(
-    proj: str, 
-    well_data: pd.DataFrame, 
-    pump_rates: pd.DataFrame, 
-    scenario_year: int, 
+    proj: str,
+    well_data: pd.DataFrame,
+    pump_rates: pd.DataFrame,
+    scenario_year: int,
     pumping_duration: int,
-    scenario_folder: str,
-    output_folder: str,
-    qa_folder: str,
-    title: str
+    plot_path: str,
+    title: str,
 ) -> pd.DataFrame:
     """
     Generate new pumping time series for the wells in the project and scenario.
@@ -88,19 +124,13 @@ def generate_pumping(
         year when scenario pumping is added
 
     pumping_duration : int
-        max number of time series steps to include
+        max number of time steps to include
 
-    scenario_folder : str
-        folder name for project scenario
-
-    output_folder : str
-        folder name for file output
-
-    qa_folder : str
-        folder name for QA plots
+    plot_path : str
+        path to save QA plot
 
     title : str
-        name of pumping scenario file
+        pumping scenario name
 
     Returns
     -------
@@ -109,11 +139,10 @@ def generate_pumping(
     """
     # get project wells. these need to be sorted by new column reference so they are written in order.
     proj_wells = well_data[well_data["Owner"] == proj].sort_values(by="ICOLWL2")
-    
+
     # create a list of pumping column references for the project wells
     proj_pumping_cols = proj_wells["ICOLWL"].tolist()
-    
-    
+
     # create a new dataframe (copy not a slice) of the pumping time series for the project wells
     df = pump_rates[["Date"] + proj_pumping_cols].copy()
 
@@ -129,7 +158,9 @@ def generate_pumping(
 
     # number of months pumping is up to the pumping_duration
     months_pumping = (
-        (max_index - first_index + 1) if (first_index + pumping_duration > max_index) else pumping_duration
+        (max_index - first_index + 1)
+        if (first_index + pumping_duration > max_index)
+        else pumping_duration
     )
 
     last_index = first_index + months_pumping
@@ -137,12 +168,14 @@ def generate_pumping(
     # get start month and day for first pumping
     month = df.iloc[first_index]["Date"].month
     day = df.iloc[first_index]["Date"].day
-    hour = df.iloc[first_index]['Date'].hour
-    minute = df.iloc[first_index]['Date'].minute
+    hour = df.iloc[first_index]["Date"].hour
+    minute = df.iloc[first_index]["Date"].minute
 
     # create template dataframe for new time series
     new_columns = pd.DataFrame(
-        data=0, index=pump_rates["Date"].to_numpy(), columns=well_data["ICOLWL2"].to_numpy()
+        data=0,
+        index=pump_rates["Date"].to_numpy(),
+        columns=well_data["ICOLWL2"].to_numpy(),
     )
     new_columns.reset_index(inplace=True)
     new_columns.rename(columns={"index": "Date"}, inplace=True)
@@ -168,16 +201,12 @@ def generate_pumping(
             nc_column,
         ] = pumping
 
+        # set the name of the qa plot
+        out_name = os.path.join(plot_path, f"{title}_{well_name}.png")
+
         # plot time series for visual QA
         plot_pumping(
-            proj,
-            well_name,
-            df,
-            col,
-            new_columns,
-            nc_column,
-            scenario_year,
-            f"{scenario_folder}/{output_folder}/{qa_folder}/{title}_{well_name}.png"
+            proj, well_name, df, col, new_columns, nc_column, scenario_year, out_name
         )
 
     return new_columns
@@ -189,17 +218,25 @@ def plot_pumping(
     orig_pump_df: pd.DataFrame,
     orig_pump_col: int,
     scenario_pump_df: pd.DataFrame,
-    scenario_pump_col: int, 
-    scenario_year: int, 
-    out_name: str
+    scenario_pump_col: int,
+    scenario_year: int,
+    out_name: str,
 ):
     """
     Plot Scenario Pumping with original pumping for QA
     """
     # plot for QA
     fig, ax = plt.subplots(figsize=(11, 6))
-    ax.plot(scenario_pump_df["Date"].to_numpy(), scenario_pump_df[scenario_pump_col].to_numpy(), label=f"{scenario_year}")
-    ax.plot(orig_pump_df["Date"].to_numpy(), orig_pump_df[orig_pump_col].to_numpy(), label="Actual")
+    ax.plot(
+        scenario_pump_df["Date"].to_numpy(),
+        scenario_pump_df[scenario_pump_col].to_numpy(),
+        label=f"{scenario_year}",
+    )
+    ax.plot(
+        orig_pump_df["Date"].to_numpy(),
+        orig_pump_df[orig_pump_col].to_numpy(),
+        label="Actual",
+    )
     ax.legend()
     ax.set_xlabel("Time")
     ax.set_ylabel("Volume (AF)")
